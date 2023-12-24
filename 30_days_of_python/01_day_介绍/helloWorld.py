d8106b69c3e25521ebbce9eb4276a82630c68f4c
zengxinbin <xinbinzeng@xyz10.com>
2023-12-24 20:39:00 +0800

    feat:day1

---
+++ 30_days_of_python/01_day_介绍/helloWorld.py
@@ -0,0 +1,16 @@
+print(2 + 3)   # 5
+print(3 - 1)   # 2
+print(2 * 3)   # 6
+print(3 / 2)   # 1.5
+print(3 ** 2)  # 9
+print(3 % 2)   # 1  整除
+print(3 // 2)  # 1  向下取整
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
