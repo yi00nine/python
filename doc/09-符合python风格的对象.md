b678a5c2cfe13e9a97e0b090ceecca561f0ed59a
zengxinbin <xinbinzeng@xyz10.com>
2023-12-24 20:30:04 +0800

    feat:流畅的py

---
+++ doc/09-符合python风格的对象.md
@@ -0,0 +1,6 @@
+###### 对象的表现形式
+- repr :需要实现__repr__
+- str :需要实现__str__
+- bytes :需要实现__bytes__
+- format :需要实现__format__
+
--- test.py
+++ test.py
@@ -1,3 +1,17 @@
+import uuid
+import time
+import hashlib
 
+uuid_str = uuid.uuid4().hex
+signTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+AK = 'super-user'
+SK = '59005119-88cf-11e8-8227-90e2ba1fdc4b'
+json_body = ''
 
-    
+print(uuid_str,json_body,signTime)
+
+signature = json_body + SK + uuid_str + signTime 
+token = hashlib.sha256(bytes(signature, encoding='utf-8')).hexdigest()
+# sha256_hash.update(signature.encode('utf-8'))
+# sha256_hex = sha256_hash.hexdigest()
+print(token)
