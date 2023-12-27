12b42b7375a1a857d9728346c256f9f03ada0393
zengxinbin <xinbinzeng@xyz10.com>
2023-12-27 14:26:21 +0800

    feat:day16

---
+++ 30_days_of_python/16_day_date时间/date.py
@@ -0,0 +1,11 @@
+from datetime import timedelta,datetime
+
+t1 = datetime(year = 2019, month = 12, day = 5, hour = 0, minute = 59, second = 0)
+t2 = datetime(year = 2020, month = 1, day = 1, hour = 0, minute = 0, second = 0)
+diff = t2 - t1
+print('Time left for new year:', diff) # Time left for new year: 26 days, 23: 01: 00
+
+t1 = timedelta(weeks=12, days=10, hours=4, seconds=20)
+t2 = timedelta(days=7, hours=5, minutes=3, seconds=30)
+t3 = t1 - t2
+print("t3 =", t3)
